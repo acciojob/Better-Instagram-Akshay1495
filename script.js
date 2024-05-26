f3960721a3f6421f76a387e66978b9c8976ca3d1
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-26 07:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function () {
+    const parent = document.getElementById('parent');
+    let draggedItem = null;
+
+    parent.addEventListener('dragstart', function (e) {
+        draggedItem = e.target;
+        setTimeout(function () {
+            draggedItem.style.display = 'none';
+        }, 0);
+    });
+
+    parent.addEventListener('dragend', function () {
+        setTimeout(function () {
+            draggedItem.style.display = 'block';
+            draggedItem = null;
+        }, 0);
+    });
+
+    parent.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    parent.addEventListener('dragenter', function (e) {
+        e.preventDefault();
+        if (e.target.classList.contains('image')) {
+            e.target.style.backgroundImage = draggedItem.style.backgroundImage;
+        }
+    });
+
+    parent.addEventListener('drop', function (e) {
+        e.preventDefault();
+        if (e.target.classList.contains('image')) {
+            draggedItem.style.backgroundImage = e.target.style.backgroundImage;
+            e.target.style.backgroundImage = null;
+        }
+    });
+});
